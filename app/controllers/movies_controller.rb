c5707a768799f64f36e5a224a4a94ebad1891bcb
Thierry Chau <thierry.chau@gmail.com>
2023-10-11 20:37:12 +0000

    refactored form

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -55,6 +55,6 @@ class MoviesController < ApplicationController
   private
 
   def movie_params
-    params.require(:movie).permit(:title, :description, :image_url)
+    params.require(:movie).permit(:title, :description, :image_url, :released_on)
   end
 end
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -5,6 +5,7 @@
 #  id          :bigint           not null, primary key
 #  description :text
 #  image_url   :string
+#  released_on :date
 #  title       :string
 #  created_at  :datetime         not null
 #  updated_at  :datetime         not null
